0b99361d11b49671a81d57e3671cdb6cf6a079a6
aj416 <ajay.ab88@gmail.com>
2023-03-07 19:19:12 +0530

    end of section 5

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,58 @@
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetail from "../details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectedActivity: () => void;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  editMode: boolean;
+  createOrEditActivity: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+}
+
+const ActivityDashboard = ({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelSelectedActivity,
+  openForm,
+  closeForm,
+  editMode,
+  createOrEditActivity,
+  deleteActivity,
+}: Props) => {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+        />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity && !editMode && (
+          <ActivityDetail
+            activity={selectedActivity}
+            cancelSelectedActivity={cancelSelectedActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && (
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+            createOrEditActivity={createOrEditActivity}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+};
+export default ActivityDashboard;
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,89 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+  createOrEditActivity: (activity: Activity) => void;
+}
+
+const ActivityForm = ({
+  activity: selectedActivity,
+  closeForm,
+  createOrEditActivity,
+}: Props) => {
+  const initialState: Activity = selectedActivity ?? {
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
+  };
+  const [activity, setActivity] = useState(initialState);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createOrEditActivity(activity);
+  };
+
+  const handleInputChange = (
+    ev: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = ev.target;
+    setActivity({ ...activity, [name]: value });
+  };
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="Title"
+          name="title"
+          value={activity.title}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          value={activity.description}
+          name="description"
+          onChange={(e) => handleInputChange(e)}
+        />
+        <Form.Input
+          placeholder="Category"
+          name="category"
+          value={activity.category}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <Form.Input
+          placeholder="Date"
+          name="date"
+          value={activity.date}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <Form.Input
+          placeholder="City"
+          name="city"
+          value={activity.city}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <Form.Input
+          placeholder="Venue"
+          name="venue"
+          value={activity.venue}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          floated="right"
+          type="button"
+          content="Cancel"
+          onClick={closeForm}
+        />
+      </Form>
+    </Segment>
+  );
+};
+export default ActivityForm;
