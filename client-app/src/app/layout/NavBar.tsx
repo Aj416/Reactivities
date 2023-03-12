4ff202efbc410c879808f4778afe0ce24a68a75c
aj416 <ajay.ab88@gmail.com>
2023-03-12 17:22:05 +0530

    end of section 10

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -14,13 +14,14 @@ const NavBar = () => {
           />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" as={NavLink} to="activities" />
+        <Menu.Item name="Activities" as={NavLink} to="/activities" />
+        <Menu.Item name="Errors" as={NavLink} to="/errors" />
         <Menu.Item>
           <Button
             positive
             content="Create Activity"
             as={NavLink}
-            to="createActivity"
+            to="/createActivity"
           />
         </Menu.Item>
       </Container>
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
 import ActivitySore from "./activityStore";
+import CommonStore from "./commonStore";
 
 interface Store {
   activityStore: ActivitySore;
+  commonStore: CommonStore;
 }
 
 export const store: Store = {
   activityStore: new ActivitySore(),
+  commonStore: new CommonStore(),
 };
 
 export const StoreContext = createContext(store);
